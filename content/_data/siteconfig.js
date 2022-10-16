d1c0548ba212da72fc814b1f3ed7da790bdb74af
Evelyn Kai-Yan Liu <40916491+evelynkyl@users.noreply.github.com>
2022-10-16 09:51:06 +0200

    Update siteconfig.js

--- content/_data/siteconfig.js
+++ content/_data/siteconfig.js
@@ -2,31 +2,25 @@ require("dotenv").config();
 
 module.exports = {
     // Website title, shown in left sidebar and in page title
-    title: "{ rené.winkelmeyer }",
+    title: "{ evelyn.kyliu }",
     // Site URL to generate absolute URLs. Used across the board.
     url: process.env.URL || "http://localhost:8080",
     // Profile image for left sidebar
     image: "/assets/images/sidebar_profile.jpg",
     // Image alt text for left sidebar
-    imageAlt: "René and Einstein",
+    imageAlt: "Evelyn Kai-Yan Liu",
     // Author name, shown in left sidebar, and used in JSON-LD
-    author: "René Winkelmeyer",
+    author: "Evelyn Kai-Yan Liu",
     // Site description, shown below site image (optional)
-    description: "My kitchen sink about stuff",
+    description: "My personal website",
     // OpenGraph default image, in case you don't have an `image`
     // set in your Markdown frontmatter; relevant for social
     // sharing.
     openGraphDefaultImage: "/assets/images/opengraph.jpg",
     // GitHub ID (optional, remove it not needed), used for link in the left sidebar
-    socialGitHub: "muenzpraeger",
+    socialGitHub: "evelynkyl",
     // LinkedIn ID  (optional, remove it not needed), used for link in the left sidebar
-    socialLinkedIn: "muenzpraeger",
-    // Twitter ID  (optional, remove it not needed), used for link in the left sidebar, and for OpenGraph sharing information
-    socialTwitter: "muenzpraeger",
-    // YouTube ID/Channel  (optional, remove it not needed), used for link in the left sidebar
-    socialYouTube: "UCH60RRaY2GI9m62z1loLjcA",
-    // Google Analytics ID  (optional, remove it not needed), used for... well, Google Analytics
-    googleAnalytics: "YOUR_GA-ID",
+    socialLinkedIn: "evelynkyl",
     // Algolia-powered search  (optional, remove it not needed),
     // See: https://github.com/algolia/algoliasearch-netlify
     algoliaSearch: {
